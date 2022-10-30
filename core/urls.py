67135ffa4803a674f1456c98313e588139b23ee3
Gledi Caushaj <gledi.alb@gmail.com>
2022-10-30 12:05:16 +0100

    Add login & registration

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
+    path("accounts/", include("registration.backends.default.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
